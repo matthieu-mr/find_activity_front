2e0fd9ada59b682da8ef53ddf6664c057fdf5143
matthieu-mr <m.michon.rossel@gmail.com>
2020-08-05 14:36:18 +0200

    ajout marker

--- screens/component/MapType.js
+++ screens/component/MapType.js
@@ -3,94 +3,9 @@ import { StyleSheet, View, AsyncStorage,Container,TextInput,Dimensions,ScrollVie
 import MapView from 'react-native-maps';
 
 import { Button,Item, Input, Icon,Label } from 'native-base';
-import { Col, Row, Grid } from 'react-native-easy-grid';
-
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
-
-
-import * as Location from 'expo-location';
-
+import {Marker} from 'react-native-maps';
 
 function MapType(props) {
-  const [natureList, setListNature] = useState()
-
-  const [adress,setAdress] = useState("Saisissez votre adresse")
-  const [distance,setdisctance] = useState("10 km")
-
-
-// requete BDD
-    useEffect(()=>{
-      recupDonnée()
-    //  console.log("hello",reponse);
-    })
-
-    async function recupDonnée(){
-      var requestBDD = await fetch(`http://192.168.56.1:3000/nature`)
-      var reponse = await requestBDD.json()
-    }
-    
-// recuperation de la location
-const [location, setLocation] = useState(null);
-const [errorMsg, setErrorMsg] = useState(null);
-
-const [latitude,setLatitude] = useState();
-const [longitude,setLongitude] = useState();
-
-
-useEffect(() => {
-  (async () => {
-    let { status } = await Location.requestPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
-
-    console.log(location)
-
-    setLatitude(location.coords.latitude)
-    setLongitude(location.coords.longitude)
-    getAdressCoords(location.coords.longitude,location.coords.latitude)
-  })();
-},[]);
-
-
-let text = 'Waiting..';
-if (errorMsg) {
-  text = errorMsg;
-} else if (location) {
-  text = JSON.stringify(location);
-}
-
-
-// Ajout des inputs & saisie adresse
-
-
-
- 
-let getAdressCoords =async (lon,lat)=> {
-  let lon2 = 2.37
-  let lat2 = 48.357
-
-var rawResponse =await fetch(`https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357&type=street`)
-
-
-
-var response = await rawResponse.json();
-// var response = JSON.parse(list.getBody())
-
-let adress = response.features[0].properties.label
-setAdress(adress)
-  console.log("retour api gouv =======>",adress)
-}
-
-
-
-
-let test = ()=> {
-  getAdressCoords() ;
-}
 
 
   return (
@@ -103,13 +18,19 @@ let test = ()=> {
         <MapView style={styles.mapStyle} 
 
           initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 48.866667,
+          longitude:  2.333333,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
           }}
 
-        />
+        >
+  <Marker
+    coordinate={{latitude: 48.866667, longitude: 2.333333}}
+  />
+
+
+        </MapView>
 
     </View>
 
