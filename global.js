94c1a7437e4a29dec96b76312789aa2f5579219e
Matthieu Michon <matthieumichon@MacBook-Pro-de-Matthieu.local>
2020-09-19 18:01:31 +0200

    final

--- global.js
+++ global.js
@@ -1,3 +1,3 @@
 // global.ip = `http://192.168.1.183:3000/` //IP wifi windows
-global.ip = `http://192.168.1.174:3000/` // ip mac 
-// global.ip = `https://find-acivity.herokuapp.com/`
+//global.ip = `http://192.168.1.174:3000/` // ip mac 
+ global.ip = `https://find-acivity.herokuapp.com/`
